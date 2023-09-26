e6502a146f5b23c837db34e0846b860decb61ad8
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-09-26 17:27:12 -0500

    php stuff

--- php/src/index.php
+++ php/src/index.php
@@ -1,3 +1,19 @@
 <?php
 
 echo 'Hello World';
+
+$name = 'Brendan';
+
+$fullName = "$name Higginbottom";
+
+echo $fullName;
+?>
+
+<body>
+    <h1>Hello World</h1>
+    <h2><?= $name ?></h2>
+</body>
+
+<?php
+
+?>
